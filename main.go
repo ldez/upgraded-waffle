8820f74cc997bd42907767f67cf55dace478638d
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-05-01 01:36:48 +0200

    WIP

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func run() error {
 	fmt.Println("file=path/to/filea.go, line=10, col=4, linter=XXX, severity=error, message=sss ssssd sd")
 	fmt.Println("file=path/to/fileb.go, line=1, col=4, linter=YYY, severity=warning, message=fdsqfds fdsq")
 
-	fmt.Println(":::endgroup::")
+	fmt.Println("::endgroup::")
 
 	// fmt.Println("::remove-matcher owner=golangci-lint-action::")
 
